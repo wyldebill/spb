7a39422cc37859546102e3be21029636d3b70dbe
william austin <william.austin@solutiondesign.com>
2014-08-07 10:39:18 -0500

    initial commit

---
+++ SignalRProgres/ProgressHub.cs
@@ -0,0 +1,72 @@
+﻿
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRProgress
+{
+    //[HubName("")]
+    public class ProgressHub : Hub
+    {
+        private static string MasterId { get; set; }
+
+
+        public override Task OnConnected()
+        {
+            var connectionIdCurrentUser = Context.ConnectionId;
+
+            if (connectionIdCurrentUser != MasterId && MasterId != null)
+                Clients.Caller.youAreNotTheMaster();
+            return base.OnConnected();
+        }
+
+
+        public void Auto()
+        {
+            SetValue(15, 1000);
+            SetValue(65, 7000);
+            SetValue(85, 8000);
+            SetValue(100, 1000);
+
+            //comment
+
+
+        }
+
+        public void IAmTheMaster(string key)
+        {
+            if (key == "4679a")
+            {
+                MasterId = Context.ConnectionId;
+                Clients.Others.youAreNotTheMaster();
+            }
+          
+        }
+
+        public void SetValue(int value, int time)
+        {
+            Clients.All.SetValue(value, time);
+        }
+
+
+        public void SendMessage(string message)
+        {
+            Clients.All.RecMessage("hello" + message);
+        }
+
+        public void Set50()
+        {
+            Clients.All.SetClient50();
+        }
+
+        public void Set100()
+        {
+            Clients.All.SetClient100();
+        }
+
+        public void Set0()
+        {
+            Clients.All.SetClient0();
+        }
+    }
+}
---
+++ SignalRProgres/Scripts/progress.js
@@ -0,0 +1,97 @@
+﻿$(function() {
+
+    // step 1. get a reference to the signalr hub
+    var progHub = $.connection.progressHub;
+
+
+    
+    // step 2. set the clientside functions
+    progHub.client.RecMessage = function(msg) {
+        console.log(msg);
+    };
+
+    progHub.client.setValue = function(val, time) {
+        $('#test > span').animate({
+            width: (val + '%')
+        }, time, function() {
+            // Animation complete.
+        });
+            
+    };
+
+    progHub.client.setClient50 = function() {
+        $('#test > span').animate({
+            width: '50%'
+        }, 200, function() {
+            // Animation complete.
+        });
+    };
+
+    progHub.client.setClient100 = function () {
+        $('#test > span').animate({
+            width: '100%'
+        }, 200, function () {
+            // Animation complete.
+        });
+    };
+        
+    progHub.client.setClient0 = function () {
+        $('#test > span').animate({
+            width: '0%'
+        }, 200, function () {
+            // Animation complete.
+        });
+    };
+        
+
+    progHub.client.youAreNotTheMaster = function () {
+        $('[id$=percent]').hide();
+        $('#iAmTheMaster').hide();
+        $('#auto').hide();
+        $('#key').hide();
+    };
+
+    progHub.client.setMaster = function() {
+        $('#iAmTheMaster').css('color', 'green');
+    };
+    
+
+
+    // step 3. call start on the hub, use .done and .fail
+    $.connection.hub.logging = true;
+    $.connection.hub.start()
+    .done(function () {
+        console.log('Now connected, connection ID=' + $.connection.hub.id);
+
+        // step 4.  setup the functions to call on server
+        // this is defining messages the server can call on me
+        $('#0percent').click(function () {
+            progHub.server.set0();
+        });
+        $('#50percent').click(function () {
+            progHub.server.set50();
+        });
+        $('#100percent').click(function () {
+            progHub.server.set100();
+        });
+
+
+        $('#auto').click(function () {
+            progHub.server.auto();
+        });
+
+        $('#iAmTheMaster').click(function () {
+            var secret = $('#key').val();
+            progHub.server.iAmTheMaster(secret);
+        });
+
+
+
+
+    })
+    .fail(function() {
+         console.log('Could not Connect!');
+    });
+
+
+});
